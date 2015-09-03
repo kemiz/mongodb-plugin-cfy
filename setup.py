2e58af7aa29782547f00d2ada7d070e7047926b6
kemiz <kemimeister@gmail.com>
2015-09-03 16:18:36 +0100

    correct setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='cloudify-mongo-plugin',
     version='1.2.1',
     author='kemiz',
-    packages=['mongo_plugin'],
+    packages=['mongodb_plugin'],
     license='LICENSE',
     install_requires=[
         'cloudify-plugins-common==3.2.1',
         'requests',
-        'cloudify'
+        'cloudify==3.2.1'
     ],
     dependency_links=[
         'https://github.com/kemiz/cloudify-package-installer-plugin/archive/master.zip'
