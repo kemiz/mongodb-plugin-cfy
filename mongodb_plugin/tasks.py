76e24ebd1724248e28f20541c039880c1c95aa5f
kemiz <kemimeister@gmail.com>
2015-09-04 09:35:38 +0100

    tomcat plugin impl

--- mongodb_plugin/tasks.py
+++ mongodb_plugin/tasks.py
@@ -7,11 +7,15 @@ __author__ = 'kemi'
 
 @operation
 def set_url(**_):
-    ctx.source.instance.runtime_properties['mongo_ip_address'] = ctx.target.instance.runtime_properties['host_ip']
-    ctx.source.instance.runtime_properties['mongo_ip_port'] = ctx.target.instance.runtime_properties['host_port']
-    # run(
-    #     'ctx source instance runtime_properties mongo_ip_address $(ctx target instance host_ip)'
-    # )
-    # run(
-    #     'ctx source instance runtime_properties mongo_ip_port $(ctx target instance host_port)'
-    # )
+    try:
+        # ctx.source.instance.runtime_properties['mongo_ip_address'] = ctx.target.instance.runtime_properties.host_ip
+        # ctx.source.instance.runtime_properties['mongo_ip_port'] = ctx.target.instance.runtime_properties.host_port
+
+        run(
+            'ctx source instance runtime_properties mongo_ip_address $(ctx target instance host_ip)'
+        )
+        run(
+            'ctx source instance runtime_properties mongo_ip_port $(ctx target instance host_port)'
+        )
+    except Exception as e:
+        raise exceptions.RecoverableError('Failed to set mongo ip & port')
