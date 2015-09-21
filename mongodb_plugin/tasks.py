ef6673f119f357b542411327dbb9cd779b366c66
Christos E <christos@gigaspaces.com>
2015-09-21 13:56:05 +0100

    Update tasks.py

--- mongodb_plugin/tasks.py
+++ mongodb_plugin/tasks.py
@@ -9,15 +9,8 @@ __author__ = 'kemi'
 @operation
 def set_url(**_):
     try:
-        # ctx.source.instance.runtime_properties['mongo_ip_address'] = ctx.target.instance.runtime_properties.host_ip
-        # ctx.source.instance.runtime_properties['mongo_ip_port'] = ctx.target.instance.runtime_properties.host_port
-
-        run(
-            'ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties ip)'
-        )
-        run(
-            'ctx source instance runtime_properties mongo_ip_port $(ctx target instance runtime_properties port)'
-        )
+        ctx.source.instance.runtime_properties['mongo_ip_address'] = ctx.target.instance.runtime_properties.host_ip
+        ctx.source.instance.runtime_properties['mongo_ip_port'] = ctx.target.instance.runtime_properties.host_port
     except Exception as e:
         raise exceptions.RecoverableError('Failed to set mongo ip & port')
 
@@ -26,5 +19,5 @@ def set_url(**_):
 def install_pymongo(**_):
     """ Installs pymongo required for Diamond monitoring """
 
-    config = ['package_list']['pymongo']
+    config = {'package_list':['pymongo']}
     install_packages(config)
